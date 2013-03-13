3fe1be61046188764d0c23e1910ca9b6f33b0160
Praktikant Acc107 <p107@atradig109.informatik.tu-muenchen.de>
2013-03-13 16:41:24 +0100

    namesurname added

--- gradient_task/gradient.cu
+++ gradient_task/gradient.cu
@@ -8,9 +8,9 @@
 *
 * 
 \******* PLEASE ENTER YOUR CORRECT STUDENT LOGIN, NAME AND ID BELOW *********/
-const char* studentLogin = "p010";
-const char* studentName  = "John Doe";
-const int   studentID    = 1234567;
+const char* studentLogin = "p107";
+const char* studentName  = "Marco Servalli";
+const int   studentID    = 3626387;
 /****************************************************************************\
 *
 * In this file the following methods have to be edited or completed:
